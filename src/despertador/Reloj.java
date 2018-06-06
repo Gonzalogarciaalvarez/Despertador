e715a949431cd1bf790770e18d38a83dc2a9d502
Gonzalo <Gonzalo@192.168.0.19>
2018-06-06 18:29:06 +0200

    Commit para subir

--- src/despertador/Reloj.java
+++ src/despertador/Reloj.java
@@ -6,11 +6,46 @@
 
 package despertador;
 
+import java.util.Date;
+import java.util.Timer;
+import java.util.TimerTask;
+
 /**
  *
  * @author Gonzalo
  */
 
 public class Reloj {
+    
+    Timer timer;
+    private int horas, minutos, segundos;
+    public static String hms;
+    Alarma alrm = new Alarma();
 
+    public Reloj() {
+
+        timer = new Timer();
+
+        timer.schedule(new TicTac(), 0, 1000);
+    }
+
+    public class TicTac extends TimerTask {
+
+        @Override
+        public void run() {
+
+            Date date = new Date();
+            horas = date.getHours();
+            minutos = date.getMinutes();
+            segundos = date.getSeconds();
+
+            hms = horas + ":" + minutos + ":" + segundos;
+            
+            Display.textoHora.setText(Reloj.hms);
+            
+            if (hms.contains(alrm.getHmsa())) {
+                Sonido.ReproducirSonido("C:\\Users\\user\\Documents\\NetBeansProjects\\Despertador\\src\\despertador\\ring.wav");
+            }
+        }
+    }
 }
