21f0f268217dfe0f2ebf1bbdf4e8026fa0c6167b
Gonzalo <Gonzalo@192.168.0.19>
2018-06-06 18:28:48 +0200

    Commit para subir

--- src/despertador/Alarma.java
+++ src/despertador/Alarma.java
@@ -6,6 +6,7 @@
 
 package despertador;
 
+import javax.swing.JOptionPane;
 
 /**
  *
@@ -13,5 +14,55 @@ package despertador;
  */
 
 public class Alarma {
+    
+    static int horas;
+    static int minutos;
+    static int segundos;
+    static String hmsa = horas + ":" + minutos + ":" + segundos;
 
+    public Alarma() {
+    }
+
+    public Alarma(int horas, int minutos, int segundos) {
+        this.horas = horas;
+        this.minutos = minutos;
+        this.segundos = segundos;
+    }
+
+    public int getHoras() {
+        return horas;
+    }
+
+    public void setHoras(int horas) {
+        this.horas = horas;
+    }
+
+    public int getMinutos() {
+        return minutos;
+    }
+
+    public void setMinutos(int minutos) {
+        this.minutos = minutos;
+    }
+
+    public int getSegundos() {
+        return segundos;
+    }
+
+    public void setSegundos(int segundos) {
+        this.segundos = segundos;
+    }
+    
+
+    public static void hmsa() {
+        horas = Integer.parseInt(JOptionPane.showInputDialog("Introduzca las horas"));
+        minutos = Integer.parseInt(JOptionPane.showInputDialog("Introduzca los minutos"));
+        segundos = Integer.parseInt(JOptionPane.showInputDialog("Introduzca los segundos"));
+        hmsa = horas + ":" + minutos + ":" + segundos;
+
+    }
+
+    public String getHmsa() {
+        return hmsa;
+    }
 }
